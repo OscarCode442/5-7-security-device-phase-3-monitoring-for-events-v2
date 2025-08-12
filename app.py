b236f1e71fa3819586ab10aa1cd7af2bc4f79fc5
OscarCode442 <102172374+OscarCode442@users.noreply.github.com>
2025-08-12 12:10:50 +0000

    pi security

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set General Purpose Input Output (GPIO) Pin 4 (position 7) to be an input pin and set initial value to be pulled low (off) 
+import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
+
+GPIO.setwarnings(False)  # Ignore warnings (e.g., if GPIO was used previously)
+GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering (not BCM/GPIO numbers)
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 7 as input with pull-down resistor
+
+while True:  # Infinite loop to keep checking the pin
+    if GPIO.input(7) == GPIO.HIGH:  # Check if the button is pressed (pin goes high)
+        print("Someone has pressed the alert button!")  # Print message to console
